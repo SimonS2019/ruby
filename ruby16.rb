23dcd0e59fe465165dc5c37bf3a7fe6c6290cecd
SimonS2019 <shentiexiong@gmail.com>
2023-10-24 17:01:45 +1300

    16

---
+++ ruby16.rb
@@ -0,0 +1,12 @@
+puts "what is your first name?"
+first_name=gets.chomp
+puts "what is your last name?"
+last_name=gets.chomp
+puts "Welcome #{first_name} #{last_name}, to the playground!"
+
+
+# puts "Enter a number to multiply by 2"
+# input=gets.chomp
+# puts input.to_i*2
+
+
