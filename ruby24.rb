8801c5261509481881d1c04180f9cc15f05aced2
SimonS2019 <shentiexiong@gmail.com>
2023-10-25 15:34:04 +1300

    24

---
+++ ruby24.rb
@@ -0,0 +1,23 @@
+
+#if/else
+
+condition = true
+antother_condition = false
+
+if condition || antother_condition
+  puts "Hello World"
+else
+  puts "Goodbye World"
+end
+
+
+name = "Evgeny"
+if name == "Evgeny"
+    puts "Welcome to the program, #{name}"
+    elsif name == "Jack"
+    puts "Welcome to the program, #{name}"
+    else
+    puts "Welcome to the program, User"
+    end
+
+puts "this is outside the if statement"
